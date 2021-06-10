c43fe4f6441de012c9e33c22de183bbd6086ed51
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-10 13:38:41 +0200

    Add `AssertJ` solution

---
+++ szt-01-public/src/test/java/location/LocationServiceTestWithAssertJ.java
@@ -0,0 +1,36 @@
+package location;
+
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+@ExtendWith(SoftAssertionsExtension.class)
+public class LocationServiceTestWithAssertJ {
+
+    @Test
+    void testGetLocationsFromFile() {
+        LocationService service = new LocationService();
+        List<Location> result = service.getLocationsFromFile(Path.of("locations.csv"));
+
+        assertThat(result)
+                .hasSize(3)
+                .extracting(Location::getName, Location::getLat)
+                .contains(tuple("Equador",0.0));
+    }
+
+    @Test
+    void testGetLocationsFromFileBySoftly(SoftAssertions softly) {
+        LocationService service = new LocationService();
+        List<Location> result = service.getLocationsFromFile(Path.of("locations.csv"));
+
+        softly.assertThat(result).hasSize(3);
+        softly.assertThat(result).extracting(Location::getName).contains("Budapest");
+    }
+}
