a913e16227fffd2da0570adedf1daf8aeb1bde3d
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-16 12:23:31 +0200

    Add `Bevezetés a Spring FrameWork használatába` solution

---
+++ locations-spring-solution/locations-spring/src/main/java/locations/Location.java
@@ -0,0 +1,25 @@
+package locations;
+
+public class Location {
+
+    private Long id;
+    private String name;
+    private double lat;
+    private double lon;
+
+    public Location(String name, double lat, double lon) {
+        this.name = name;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lat=" + lat +
+                ", lon=" + lon +
+                '}';
+    }
+}
